bf35caa78ed312cfd6f854a3d03cdace1f06a7dd
Aslihan Akbiyik <48156075+aslihana@users.noreply.github.com>
2022-03-10 14:38:44 +0300

    Update my_malloc.c

--- my_malloc.c
+++ my_malloc.c
@@ -10,10 +10,50 @@ void InitMyMalloc()
 	freeList->next = NULL;
 }
 
-/*void MyMalloc()
+void *MyMalloc(size_t noOfBytes)
 {
+	struct block *current, *previous;
 
-}*/
+	void *result;
+
+	if(!(freeList->size))
+	{
+		InitMyMalloc();
+		printf("Memory has initialized \n");
+	}
+
+	current = freeList;
+
+	while((((current->size) < noOfBytes) || ((current->free) == false)) && (current->next != NULL))
+	{
+		previous = current;
+
+		current = current->next;
+
+		printf("One block has checked \n");
+	}
+
+	if((current->size) == noOfBytes)
+	{
+		current->free = false;
+
+		result = (void*)(++current);
+
+		printf("Exact fitting block has allocated \n");
+
+		return result;
+	}
+
+	else if((current->size) > (noOfBytes + sizeof(struct block)))
+	{
+		//split the current block
+	}
+
+	else
+	{
+		
+	}
+}
 
 void MyFree(void* list)
 {
